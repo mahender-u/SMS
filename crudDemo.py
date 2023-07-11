5194d3dab4f78ba5fc71023a7f20aa1fe077ba11
mahender-u <127581860+mahender-u@users.noreply.github.com>
2023-07-11 07:00:35 +0530

    Add files via upload

---
+++ crudDemo.py
@@ -0,0 +1,13 @@
+from tkinter import*
+import mysql.connector as mysql
+import tkinter.messagebox as msg  
+
+root=Tk()
+root.geometry("500x300")
+root.title("Curd operations")
+id=Label(root,text="Enter id:")
+id.place(x=50,y=30)
+
+
+root.mainloop
+
